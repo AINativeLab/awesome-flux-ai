2e29722fa048752672a0cd10cf9d3d68ff2e91aa
Airyland <i@mao.li>
2024-08-18 02:42:00 +0800

    tweak .gitignore

--- generate-html.js
+++ generate-html.js
@@ -1,13 +1,15 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+const config = require('./config.json')
+const cheerio = require('cheerio')
 
-const readmePath = path.join(__dirname, 'README.md');
-const markdown = fs.readFileSync(readmePath, 'utf-8');
+const readmePath = path.join(__dirname, 'README.md')
+const markdown = fs.readFileSync(readmePath, 'utf-8')
 
 const faqData = [
-    {
-        question: "How do I submit a new resource?",
-        answer: `To submit a new resource to the Awesome Flux AI list, please follow these steps:
+  {
+    question: 'How do I submit a new resource?',
+    answer: `To submit a new resource to the Awesome Flux AI list, please follow these steps:
             <ol class="list-decimal list-inside space-y-2">
                 <li>Ensure your resource is related to Flux AI technology.</li>
                 <li>Fork the <a href="https://github.com/AINativeLab/awesome-flux-ai" target="_blank" rel="noopener noreferrer" class="text-blue-600 hover:underline">GitHub repository</a>.</li>
@@ -16,68 +18,170 @@ const faqData = [
                 <li>Wait for the maintainers to review and merge your PR.</li>
             </ol>
             <p class="mt-2">We appreciate your contributions to the Flux AI community!</p>`
-    },
-    {
-        question: "What is Flux AI?",
-        answer: "Flux AI is a suite of open-source text-to-image AI models developed by Black Forest Labs. It aims to advance state-of-the-art generative deep learning models for media, pushing the boundaries of creativity, efficiency, and diversity."
-    },
-    {
-        question: "How can I use Flux AI models?",
-        answer: "Flux AI models can be used through various platforms and APIs. Some models are available for commercial use, while others are for development purposes only. Check the specific model's license and usage instructions before implementing. You can find links to different Flux AI models and applications in the resources listed above."
-    },
-    {
-        question: "How often is this list updated?",
-        answer: "This list is updated regularly as new resources and tools become available. Community contributions play a significant role in keeping the list up-to-date. If you notice any outdated information or have a new resource to add, feel free to submit an update via a Pull Request to the GitHub repository. The frequency of updates may vary depending on the pace of developments in the Flux AI ecosystem."
-    }
-];
+  },
+  {
+    question: 'What is Flux AI?',
+    answer:
+      'Flux AI is a suite of open-source text-to-image AI models developed by Black Forest Labs. It aims to advance state-of-the-art generative deep learning models for media, pushing the boundaries of creativity, efficiency, and diversity.'
+  },
+  {
+    question: 'How can I use Flux AI models?',
+    answer:
+      "Flux AI models can be used through various platforms and APIs. Some models are available for commercial use, while others are for development purposes only. Check the specific model's license and usage instructions before implementing. You can find links to different Flux AI models and applications in the resources listed above."
+  },
+  {
+    question: 'How often is this list updated?',
+    answer:
+      'This list is updated regularly as new resources and tools become available. Community contributions play a significant role in keeping the list up-to-date. If you notice any outdated information or have a new resource to add, feel free to submit an update via a Pull Request to the GitHub repository. The frequency of updates may vary depending on the pace of developments in the Flux AI ecosystem.'
+  }
+]
 
-function parseMarkdown(md) {
-    const sections = md.split('\n## ').slice(1);
-    return sections.map(section => {
-        const [title, ...content] = section.split('\n');
-        const links = content
-            .filter(line => line.trim().startsWith('-'))
-            .map(line => {
-                const match = line.match(/\[(.+)\]\((.+)\)/);
-                return match ? { title: match[1], url: match[2] } : null;
-            })
-            .filter(Boolean);
-        return { title, content: content.join('\n'), links };
-    }).filter(section => {
-      console.log(section)
+async function parseMarkdown(md) {
+  const sections = md.split('\n## ').slice(1)
+  const list = sections
+    .map((section) => {
+      const [title, ...content] = section.split('\n')
+      const links = content
+        .filter((line) => line.trim().startsWith('-'))
+        .map((line) => {
+          const match = line.match(/\[(.+)\]\((.+)\)/)
+          return match ? { title: match[1], url: match[2] } : null
+        })
+        .filter(Boolean)
+      return { title, content: content.join('\n'), links }
+    })
+    .filter((section) => {
       return section.title !== 'Online Demos'
     })
+
+  for (const section of list) {
+    for (link of section.links) {
+      if (link.url.startsWith('https://replicate.com/')) {
+        if (config.examples[link.title]) {
+          console.log('skip')
+        } else {
+          console.log('go get')
+          const rs = await fetch(link.url, {
+            headers: {
+              'user-agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+            }
+          })
+          const text = await rs.text()
+          const $ = cheerio.load(text)
+
+          const imageList = $('img[role="presentation"]')
+            .map((index, element) => ({
+              src: $(element).attr('data-src')
+            }))
+            .get()
+            .map((one) => one.src)
+            .filter((one) => !!one)
+
+          config.examples[link.title] = imageList
+          fs.writeFileSync('./config.json', JSON.stringify(config, null, 2))
+        }
+      } else if (link.url.startsWith('https://huggingface.co/')) {
+        if (config.examples[link.title]) {
+          console.log('skip')
+        } else {
+          const rs = await fetch(link.url, {
+            headers: {
+              'user-agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+            }
+          })
+          const text = await rs.text()
+          const $ = cheerio.load(text)
+
+          const imageList = $('figure img')
+            .map((index, element) => ({
+              src: $(element).attr('src')
+            }))
+            .get()
+            .map((one) => one.src)
+            .filter((one) => !!one)
+
+          config.examples[link.title] = imageList
+          fs.writeFileSync('./config.json', JSON.stringify(config, null, 2))
+        }
+      }
+    }
+  }
+
+  return list
 }
 
 function truncateString(str, num) {
-    if (str.length <= num) {
-        return str;
-    }
-    return str.slice(0, num) + '...';
+  if (str.length <= num) {
+    return str
+  }
+  return str.slice(0, num) + '...'
 }
 
-function createCard(link) {
-    const domain = new URL(link.url).hostname;
-    const truncatedTitle = truncateString(link.title, 30);
-    return `
-        <a href="${link.url}" target="_blank" rel="noopener noreferrer" class="bg-white rounded-lg shadow-sm p-4 flex flex-col h-full hover:shadow-md transition-shadow duration-300">
-            <div class="flex items-start mb-2">
-                <img src="https://favicon.im/${domain}?larger=true" alt="Icon" class="w-12 h-12 mr-3 rounded">
-                <div class="flex-grow">
-                    <h3 class="text-lg font-semibold" title="${link.title}">${truncatedTitle}</h3>
-                    <span class="inline-block text-gray-500 text-xs">${domain}</span>
-                </div>
-            </div>
-            <!--<p class="text-sm text-gray-600 mt-2 flex-grow">Explore Flux AI resources and tools.</p>-->
-        </a>
-    `;
+function createCard(link, section) {
+  const domain = new URL(link.url).hostname
+  const truncatedTitle = truncateString(link.title, 40)
+  const examples = config.examples[link.title] || []
+  const exampleImages = examples
+    .slice(0, 4)
+    .map(
+      (img, index) =>
+        `<div class="aspect-square overflow-hidden">
+           <img onerror="this.src='${img}'" src="${
+          img
+            .replace('https://huggingface.co/', 'https://25e472d.webp.li/')
+            .replace(
+              'https://replicate.delivery/',
+              'https://a6e770d.webp.li/'
+            ) + '?width=256'
+        }" 
+           alt="Example ${
+             index + 1
+           }" class="w-full h-full object-cover rounded">
+         </div>`
+    )
+    .join('')
+
+  return `
+    <a href="${
+      link.url
+    }" target="_blank" rel="noopener" class="bg-white rounded-lg shadow-sm p-4 flex h-full hover:shadow-md transition-shadow duration-300">
+      <div class="flex-grow pr-4">
+        <div class="flex items-start mb-3">
+          ${
+            section.title === 'Apps and websites'
+              ? `<img src="https://favicon.im/${domain}?larger=true" alt="Icon" class="w-12 h-12 mr-3 rounded">`
+              : ''
+          } 
+          <div>
+            <h3 class="text-xl font-medium leading-tight mb-1" title="${
+              link.title
+            }">${truncatedTitle}</h3>
+            <span class="inline-block text-gray-500 text-sm">${domain}</span>
+          </div>
+        </div>
+      </div>
+      ${
+        section.title === 'Flux LoRA Collection' && exampleImages
+          ? `
+        <div class="flex-shrink-0 grid grid-cols-2 gap-1 w-32 h-32">
+          ${exampleImages}
+        </div>
+      `
+          : ''
+      }
+    </a>
+  `
 }
 
 function createFAQSection(faqData) {
-    return `
+  return `
         <div class="bg-white rounded-lg shadow-sm p-6 mb-8">
             <h2 class="text-2xl font-bold mb-6">FAQ</h2>
-            ${faqData.map((item, index) => `
+            ${faqData
+              .map(
+                (item, index) => `
                 <div class="mb-4 border-b border-gray-200 pb-4 last:border-b-0 last:pb-0">
                     <button class="faq-question text-left w-full flex justify-between items-center py-2 px-4 bg-gray-50 hover:bg-gray-100 rounded-lg transition-colors duration-200" onclick="toggleAnswer(${index})">
                         <span class="font-semibold text-gray-800">${item.question}</span>
@@ -89,30 +193,39 @@ function createFAQSection(faqData) {
                         ${item.answer}
                     </div>
                 </div>
-            `).join('')}
+            `
+              )
+              .join('')}
         </div>
-    `;
+    `
 }
 
 function generateHTML(parsedSections) {
-    let content = '';
+  let content = ''
 
-    parsedSections.forEach(section => {
-        if (section.links.length > 0) {
-            content += `
+  parsedSections.forEach((section) => {
+    if (section.links.length > 0) {
+      content += `
                 <div class="mb-8">
-                    <h2 class="text-2xl font-bold mb-4">${section.title}</h2>
-                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                        ${section.links.map(link => createCard(link)).join('')}
+                    <h2 class="text-2xl font-bold mb-4">${section.title}</h2>`
+
+      if (section.title !== 'Flux LoRA Collection') {
+        content += `<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">`
+      } else {
+        content += `<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">`
+      }
+      content += `${section.links
+        .map((link) => createCard(link, section))
+        .join('')}
                     </div>
                 </div>
-            `;
-        }
-    });
+            `
+    }
+  })
 
-    content += createFAQSection(faqData);
+  content += createFAQSection(faqData)
 
-    return `
+  return `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -184,13 +297,15 @@ function generateHTML(parsedSections) {
     </script>
 </body>
 </html>
-    `;
+    `
 }
 
-const parsedSections = parseMarkdown(markdown);
-const html = generateHTML(parsedSections);
+;(async () => {
+  const parsedSections = await parseMarkdown(markdown)
+  const html = generateHTML(parsedSections)
 
-const outputPath = path.join(__dirname, 'index.html');
-fs.writeFileSync(outputPath, html);
+  const outputPath = path.join(__dirname, 'index.html')
+  fs.writeFileSync(outputPath, html)
 
-console.log('HTML file generated successfully!');
+  console.log('HTML file generated successfully!')
+})()
